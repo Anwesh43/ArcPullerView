081c179634131da7d39997e5ef28073c9dc2ef4b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-18 10:45:01 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedarcpullerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedarcpullerview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedarcpullerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.arcpullerview.ArcPullerView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ArcPullerView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
