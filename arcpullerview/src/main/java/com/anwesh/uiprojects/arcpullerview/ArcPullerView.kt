dd295f62eb63b7e2508d35990d1eefc1238cbbab
Anwesh43 <anweshthecool0@gmail.com>
2019-11-18 09:37:15 +0530

    adding Int and Float extension methods

--- arcpullerview/src/main/java/com/anwesh/uiprojects/arcpullerview/ArcPullerView.kt
+++ arcpullerview/src/main/java/com/anwesh/uiprojects/arcpullerview/ArcPullerView.kt
@@ -20,3 +20,9 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify(n : Int) : Float = Math.sin(this * (Math.PI / n)).toFloat()
+fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (Math.PI / 2) * this).toFloat()
