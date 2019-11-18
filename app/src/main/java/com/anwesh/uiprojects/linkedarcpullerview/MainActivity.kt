09d7cb216015a269de0850a26ca70448ea72c170
Anwesh43 <anweshthecool0@gmail.com>
2019-11-18 10:42:36 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedarcpullerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedarcpullerview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedarcpullerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.arcpullerview.ArcPullerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArcPullerView.create(this)
     }
 }
